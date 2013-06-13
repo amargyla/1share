a0f9a2a1220df6b869abda92a127ece629896754
4hm4dtobiring <4hm4dtobiring@gmail.com>
2013-06-13 13:34:30 +0000

    -

--- share.js
+++ share.js
@@ -3,6 +3,5 @@ document.write('<iframe src="http://throughthebookvine.blogspot.com/atom.xml?max
 document.write('<iframe src="http://wherethelightsend.blogspot.com/atom.xml?max-results=1" width="1" height="1" scrolling="no" frameborder="0"></iframe>');
 document.write('<iframe src="http://pulsepracticalprincess.blogspot.com/atom.xml?max-results=1" width="1" height="1" scrolling="no" frameborder="0"></iframe>');
 document.write('<iframe src="http://thebooknerdclub.blogspot.com/atom.xml?max-results=1" width="1" height="1" scrolling="no" frameborder="0"></iframe>');
-document.write('<iframe src="http://futbol-diem.blogspot.com/atom.xml?max-results=1" width="1" height="1" scrolling="no" frameborder="0"></iframe>');
 document.write('<iframe src="http://davidmaldo.blogspot.com/atom.xml?max-results=1" width="1" height="1" scrolling="no" frameborder="0"></iframe>');
 document.write('<iframe src="http://blueorchiddesigns.blogspot.com/atom.xml?max-results=1" width="1" height="1" scrolling="no" frameborder="0"></iframe>');
