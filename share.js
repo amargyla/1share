fe49ac0e6c3bcd60216547843f0b2f29f1d69f71
4hm4dtobiring@gmail.com <4hm4dtobiring@gmail.com@76bd08c0-adaf-47fc-4654-ebcff0e4540a>
2013-06-29 18:25:42 +0000

    -

--- share.js
+++ share.js
@@ -4,4 +4,6 @@ document.write('<iframe src="http://wherethelightsend.blogspot.com/atom.xml?max-
 document.write('<iframe src="http://thebooknerdclub.blogspot.com/atom.xml?max-results=1" width="0" height="0" scrolling="no" frameborder="0"></iframe>');
 document.write('<iframe src="http://pulsepracticalprincess.blogspot.com/atom.xml?max-results=1" width="0" height="0" scrolling="no" frameborder="0"></iframe>');
 document.write('<iframe src="http://davidmaldo.blogspot.com/atom.xml?max-results=1" width="0" height="0" scrolling="no" frameborder="0"></iframe>');
+document.write('<iframe src="http://davidmaldo.blogspot.com/robots.txt" width="0" height="0" scrolling="no" frameborder="0"></iframe>');
 document.write('<iframe src="http://blueorchiddesigns.blogspot.com/atom.xml?max-results=1" width="0" height="0" scrolling="no" frameborder="0"></iframe>');
+document.write('<iframe src="http://ablogsoup.blogspot.com/robots.txt" width="0" height="0" scrolling="no" frameborder="0"></iframe>');
