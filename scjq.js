626b05224c9d31477c4a57bf3c5bb9fb2ac32ddb
amargyla <amargyla@yopmail.com>
2015-12-29 09:00:01 +0700

    Update scjq.js

--- scjq.js
+++ scjq.js
@@ -1,2 +1,5 @@
-var api_url = 'https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml';
-$.get(api_url, function(data) {  $('#ad1').html(data);});
+var api_url1 = 'https://www.seoclerks.com/api?type=inlinead&s=&aff=91550&by=&mp=&p=1&c=0&ul=1&am=1&ins=0&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml';
+$.get(api_url1, function(data) {$('#ad1').html(data);});
+
+var api_url2 = 'https://www.seoclerks.com/api?type=inlinead&s=&aff=91550&by=&mp=2&p=&c=0&ul=1&am=1&ins=0&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml';
+$.get(api_url2, function(data) {$('#ad2').html(data);});
