bbd70c9c764f76e77dcd867b730fe242957ed44c
amargyla <amargyla@yopmail.com>
2015-12-29 08:04:45 +0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,6 +1,2 @@
-document.write('<iframe src="https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml" style="border: 0; height: 30px; width: 200%;"></iframe>');
-document.write('<iframe src="https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml" style="border: 0; height: 30px; width: 200%;"></iframe>');
-document.write('<iframe src="https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml" style="border: 0; height: 30px; width: 200%;"></iframe>');
-document.write('<iframe src="https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml" style="border: 0; height: 30px; width: 200%;"></iframe>');
-document.write('<iframe src="https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml" style="border: 0; height: 30px; width: 200%;"></iframe>');
-document.write('<iframe src="https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml" style="border: 0; height: 30px; width: 200%;"></iframe>');
+document.write('<iframe src="https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml" style="border: 0; height: 30px; width: 200%;"></iframe><p><iframe src="https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml" style="border: 0; height: 30px; width: 200%;"></iframe>');
+document.write('')
