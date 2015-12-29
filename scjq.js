05c73538251946cb4591220ac8fa667236c79265
amargyla <amargyla@yopmail.com>
2015-12-29 08:44:19 +0700

    Update scjq.js

--- scjq.js
+++ scjq.js
@@ -1,4 +1,2 @@
 var api_url = 'https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml';
-$.get(api_url, function(data) {
-  $('#ad1').html(data);
-});
+$.get(api_url, function(data) {  $('#ad1').html(data);});
