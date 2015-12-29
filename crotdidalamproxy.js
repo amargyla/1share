b7593d257cc9ba75e728ac68f2c7459d5ed048ee
amargyla <amargyla@yopmail.com>
2015-12-29 07:22:29 +0700

    Update crotdidalamproxy.js

--- crotdidalamproxy.js
+++ crotdidalamproxy.js
@@ -1,1 +1,6 @@
-document.write('<div id="cddproxo"><iframe allowtransparency="true" frameborder="0" id="cddproxi" scrolling="no" src="http://www.ip-adress.com/proxy_list/?k=time&amp;d=desc"></iframe></div>');
+document.write('<span id="ad1"></span> <script type="application/javascript">
+var api_url = 'https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml';
+$.get(api_url, function(data) {
+  $('#ad1').html(data);
+});
+</script>');
