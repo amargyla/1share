a98cbf57713f420c7e8639196e53854682cd0f09
amargyla <amargyla@yopmail.com>
2015-12-29 08:13:47 +0700

    Create scjq.js

---
+++ scjq.js
@@ -0,0 +1,8 @@
+<span id="ad1"></span>
+
+<script type="application/javascript">
+var api_url = 'https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml';
+$.get(api_url, function(data) {
+  $('#ad1').html(data);
+});
+</script>
