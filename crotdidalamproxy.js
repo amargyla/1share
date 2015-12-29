baba2700f7770c7b9ccc588d884c8098790d4ad8
amargyla <amargyla@yopmail.com>
2015-12-29 07:26:35 +0700

    Update crotdidalamproxy.js

--- crotdidalamproxy.js
+++ crotdidalamproxy.js
@@ -1,6 +1,8 @@
-document.write('<span id="ad1"></span> <script type="application/javascript">
+<span id="ad1"></span>
+
+<script type="application/javascript">
 var api_url = 'https://www.seoclerks.com/api?type=inlinead&s=&aff=1&by=&c=0&ul=1&am=1&g=0&sub=0&os=0&sp=0&oby=&oh=&f=singlehtml';
 $.get(api_url, function(data) {
   $('#ad1').html(data);
 });
-</script>');
+</script>
